48cc81c801e9a3f2044934d3106ac5cac48fff9e
sgolovine <sunny@glvn.co>
2018-04-09 00:48:10 -0400

    Added patcher script

---
+++ patcher.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+## OTF FONTS
+# IBMPlexMono-Bold.otf
+# IBMPlexMono-BoldItalic.otf
+# IBMPlexMono-ExtraLight.otf
+# IBMPlexMono-ExtraLightItalic.otf
+# IBMPlexMono-Italic.otf
+# IBMPlexMono-Light.otf
+# IBMPlexMono-LightItalic.otf
+# IBMPlexMono-Medium.otf
+# IBMPlexMono-MediumItalic.otf
+# IBMPlexMono-Regular.otf
+# IBMPlexMono-SemiBold.otf
+# IBMPlexMono-SemiBoldItalic.otf
+# IBMPlexMono-Text.otf
+# IBMPlexMono-TextItalic.otf
+# IBMPlexMono-Thin.otf
+# IBMPlexMono-ThinItalic.otf
+
+## TTF FONTS
+# IBMPlexMono-Bold.ttf
+# IBMPlexMono-BoldItalic.ttf
+# IBMPlexMono-ExtraLight.ttf
+# IBMPlexMono-ExtraLightItalic.ttf
+# IBMPlexMono-Italic.ttf
+# IBMPlexMono-Light.ttf
+# IBMPlexMono-LightItalic.ttf
+# IBMPlexMono-Medium.ttf
+# IBMPlexMono-MediumItalic.ttf
+# IBMPlexMono-Regular.ttf
+# IBMPlexMono-SemiBold.ttf
+# IBMPlexMono-SemiBoldItalic.ttf
+# IBMPlexMono-Text.ttf
+# IBMPlexMono-TextItalic.ttf
+# IBMPlexMono-Thin.ttf
+# IBMPlexMono-ThinItalic.ttf
+
+## SAMPLE CODE
+# /usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Regular.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+
+# OTF
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Bold.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-BoldItalic.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-ExtraLight.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-ExtraLightItalic.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Italic.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Light.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-LightItalic.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Medium.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-MediumItalic.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Regular.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-SemiBold.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-SemiBoldItalic.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Text.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-TextItalic.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Thin.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-ThinItalic.otf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/otf/.
+
+#TTF
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Bold.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-BoldItalic.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-ExtraLight.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-ExtraLightItalic.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Italic.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Light.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-LightItalic.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Medium.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-MediumItalic.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Regular.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-SemiBold.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-SemiBoldItalic.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Text.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-TextItalic.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Thin.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-ThinItalic.ttf -s -l -c -out /Users/sgolovine/Code/PlexNerdfont/patched/ttf/.
+
+
+### WINDOWS COMPATABLE 
+
+
+# OTF
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Bold.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-BoldItalic.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-ExtraLight.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-ExtraLightItalic.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Italic.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Light.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-LightItalic.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Medium.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-MediumItalic.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Regular.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-SemiBold.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-SemiBoldItalic.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Text.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-TextItalic.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-Thin.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/otf/IBMPlexMono-ThinItalic.otf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/otf/.
+
+#TTF
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Bold.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-BoldItalic.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-ExtraLight.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-ExtraLightItalic.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Italic.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Light.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-LightItalic.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Medium.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-MediumItalic.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Regular.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-SemiBold.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-SemiBoldItalic.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Text.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-TextItalic.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-Thin.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
+
+/usr/local/bin/fontforge -script /Users/sgolovine/Code/nerd-fonts/font-patcher /Users/sgolovine/Code/PlexNerdfont/originals/ttf/IBMPlexMono-ThinItalic.ttf -s -l -w -c -out /Users/sgolovine/Code/PlexNerdfont/windows-compatable/ttf/.
